819b23963c98e513af00ec30a07a4a216c548e01
xuuHuang <hxx2904@outlook.com>
2024-12-25 21:15:16 +0800

    delete lcb and ruler

--- RULER/scripts/run_multilingual.sh
+++
@@ -1,5 +0,0 @@
-set -e
-for lang in en zh es fr de ru ja th sw bn te ar ko vi cs hu sr; do
-# for lang in cs hu sr; do
-    bash run.sh llama3.1-70b-chat synthetic $lang
-done
--- RULER/scripts/run_qwen_mul.sh
+++
@@ -1,4 +0,0 @@
-set -e
-for lang in en zh es fr de ru ja th sw bn te ar ko vi cs hu sr; do
-    bash run.sh qwen2.5-72b-chat synthetic $lang
-done
