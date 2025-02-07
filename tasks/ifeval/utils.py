14ff708488e8a19a84d9291326a66c5e6737bcf7
xuuHuang <hxx2904@outlook.com>
2025-02-07 20:13:53 +0800

    update ifeval utils

--- tasks/ifeval/utils.py
+++ tasks/ifeval/utils.py
@@ -2,7 +2,6 @@ import os
 import sys
 print(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-# sys.path.append("/nas/shared/NLP_A100/huangxu/xeval/tasks/xeval/ifeval")
 import dataclasses
 from typing import Dict, Optional, Union
 
@@ -141,15 +140,3 @@ def agg_inst_level_acc(items):
     inst_level_acc = sum(flat_items) / len(flat_items)
     return inst_level_acc
 
-if __name__ == "__main__":
-    doc = {"key": 1609, "prompt": "有什么好办法可以约 Sonia 出去？请用 4 段文字回复，每段用两行隔开。请用双引号将整个回复括起来。第一段必须以单词“周末”开头。", "instruction_id_list": ["length_constraints:nth_paragraph_first_word", "startend:quotation"], "kwargs": [{"num_highlights": None, "relation": None, "num_words": None, "num_placeholders": None, "prompt_to_repeat": None, "num_bullets": None, "keywords": None, "num_paragraphs": 4, "language": None, "section_spliter": None, "num_sections": None, "end_phrase": None, "forbidden_words": None, "keyword": None, "frequency": None, "postscript_marker": None, "num_sentences": None, "first_word": "周末", "nth_paragraph": 1}, {"num_highlights": None, "relation": None, "num_words": None, "num_placeholders": None, "prompt_to_repeat": None, "num_bullets": None, "keywords": None, "num_paragraphs": None, "language": None, "section_spliter": None, "num_sections": None, "end_phrase": None, "forbidden_words": None, "keyword": None, "frequency": None, "postscript_marker": None, "num_sentences": None, "first_word": None, "nth_paragraph": None}], "lang": "zh-CN"}
-    resp = "\"周末的好时光，适合出去玩耍。可以邀请Sonia去公园或是游乐场，享受美好的时光。\"\n\n\"或者可以邀请她去看电影或是听音乐会，享受艺术的魅力。\"\n\n\"如果Sonia喜欢运动，可以邀请她去打球或是去健身房，锻炼身体。\"\n\n\"最后，可以邀请她去吃饭或是喝咖啡，享受美食和温馨的时光。\""
-    inp = InputExample(
-        key=doc["key"],
-        instruction_id_list=doc["instruction_id_list"],
-        prompt=doc["prompt"],
-        kwargs=doc["kwargs"],
-        lang=doc.get("lang", "en")
-    )
-    out_loose = test_instruction_following_loose(inp, resp)
-    print(out_loose)
