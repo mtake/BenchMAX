bd2d76b7ef097090c7842a952bf91ceb44932175
Mikio Takeuchi <mtake@jp.ibm.com>
2025-05-28 07:38:10 -0400

    update script

--- run_mtake.sh
+++ run_mtake.sh
@@ -17,14 +17,15 @@ echo "XXX LOGFILE ${LOGFILE}" | tee -a ${LOGFILE}
 echo "XXX DATETIME ${START_TIME_STR}" | tee -a ${LOGFILE}
 
 LANGS=()
-LANGS+=("en")
 #LANGS+=("en,ja")
+#LANGS+=("en")
+LANGS+=("ja")
 MODELS=()
-#MODELS+=("ibm-granite/granite-3.3-8b-instruct")
+MODELS+=("ibm-granite/granite-3.3-8b-instruct")
 MODELS+=("granite-3.3-8b-instruct-teigaku-genzei-interp")
 MODELS+=("granite-3.3-8b-instruct-teigaku-genzei")
 TASKS=()
-TASKS+=("science")
+TASKS+=("xgpqa")
 
 for l in "${LANGS[@]}"; do
     for m in "${MODELS[@]}"; do
@@ -33,7 +34,6 @@ for l in "${LANGS[@]}"; do
 	    THIS_START_TIME_STR="$(${DATE_CMD} -d @${THIS_START_TIME} +%Y%m%d-%H%M%S)"
 	    echo "XXX THIS_DATETIME ${THIS_START_TIME_STR}" | tee -a ${LOGFILE}
 
-	    #cmd="/usr/bin/env bash ./run.sh ${m} ${t} ${l}"
 	    cmd="./run.sh ${m} ${t} ${l}"
 	    echo "$cmd" | tee -a ${LOGFILE}
 	    eval "$cmd" 2>&1 | tee -a ${LOGFILE}
