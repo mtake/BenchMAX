c408fa641a736c124a07fbb1ddb0001b58d1f806
Mikio Takeuchi <mtake@jp.ibm.com>
2025-07-23 04:48:54 -0400

    WIP multi-gpu

--- run_mtake.sh
+++ run_mtake.sh
@@ -27,8 +27,8 @@ MODELS=()
 # MODELS+=("granite-3.3-8b-instruct-ibm-newsroom-d5-x100-interp")
 # MODELS+=("granite-3.3-8b-instruct-ibm-newsroom-d5-x100")
 # MODELS+=("granite-3.3-8b-instruct-jfe-technical-report_r5-interp" "granite-3.3-8b-instruct-jfe-technical-report_r5")
-MODELS+=("granite-4.0-tiny-prerelease-greylock-r250721a")
-# MODELS+=("granite-4.0-small-prerelease-greylock-r250721a")
+# MODELS+=("granite-4.0-tiny-prerelease-greylock-r250721a")
+MODELS+=("granite-4.0-small-prerelease-greylock-r250721a")
 
 TASKS=()
 TASKS+=("xgpqa")
@@ -36,6 +36,7 @@ TASKS+=("xgpqa")
 ENV=""
 #ENV="TOKENIZERS_PARALLELISM=false ${ENV}"
 ENV="PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True ${ENV}"
+#ENV="VLLM_WORKER_MULTIPROC_METHOD=spawn ${ENV}"  # @@@ahoaho XXX WIP
 
 for l in "${LANGS[@]}"; do
     for m in "${MODELS[@]}"; do
