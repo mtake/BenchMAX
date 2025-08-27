d4996c2b3c21f6d61376619738dd109ae6a41a7b
Mikio Takeuchi <mtake@jp.ibm.com>
2025-08-27 12:58:59 -0400

    add granite-3.3-8b-instruct_teigaku-genzei-sdg_hub_v0.2

--- run_mtake.sh
+++ run_mtake.sh
@@ -50,7 +50,8 @@ MODELS=()
 # MODELS+=("mistralai/Mistral-Small-3.2-24B-Instruct-2506")  # 2 GPUs. WIP need special flags to vLLM
 # MODELS+=("granite-3.3-8b-instruct_teigaku-genzei-interp" "granite-3.3-8b-instruct_teigaku-genzei")
 # MODELS+=("granite-3.3-8b-instruct_teigaku-genzei-ibm_generic_tmpl-interp" "granite-3.3-8b-instruct_teigaku-genzei-ibm_generic_tmpl")
-MODELS+=("openai/gpt-oss-20b")  # WIP
+# MODELS+=("openai/gpt-oss-20b")  # WIP
+MODELS+=("granite-3.3-8b-instruct_teigaku-genzei-sdg_hub_v0.2-interp" "granite-3.3-8b-instruct_teigaku-genzei-sdg_hub_v0.2")
 
 TASKS=()
 TASKS+=("xgpqa")
