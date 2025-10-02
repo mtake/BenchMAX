1789b1fca5bc2c4d770d8107f514d61620ec4c66
Mikio Takeuchi <mtake@jp.ibm.com>
2025-10-01 23:43:37 -0400

    add granite-3.3-8b-instruct_teigaku-genzei-ibm-v6_osft

--- run_mtake.sh
+++ run_mtake.sh
@@ -52,7 +52,8 @@ MODELS=()
 # MODELS+=("granite-3.3-8b-instruct_teigaku-genzei-ibm_generic_tmpl-interp" "granite-3.3-8b-instruct_teigaku-genzei-ibm_generic_tmpl")
 # MODELS+=("openai/gpt-oss-20b")  # WIP
 # MODELS+=("granite-3.3-8b-instruct_teigaku-genzei-v0.2-interp" "granite-3.3-8b-instruct_teigaku-genzei-v0.2")
-MODELS+=("granite-3.3-8b-instruct_teigaku-genzei-ibm-v6-interp" "granite-3.3-8b-instruct_teigaku-genzei-ibm-v6")  # v0.2
+# MODELS+=("granite-3.3-8b-instruct_teigaku-genzei-ibm-v6-interp" "granite-3.3-8b-instruct_teigaku-genzei-ibm-v6")  # v0.2
+MODELS+=("granite-3.3-8b-instruct_teigaku-genzei-ibm-v6_osft" "granite-3.3-8b-instruct_teigaku-genzei-ibm-v6_sft")  # v0.2
 
 TASKS=()
 TASKS+=("xgpqa")
